e7b67be9490cb4a0105e306c792eb9c95d950ec0
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-12 15:54:56 +0200

    dictionaries

--- ad_sets.py
+++ ad_sets.py
@@ -1,2 +1,35 @@
-art_friends = {'Rolf', 'Kamil', 'Juras'}
-science_firends = {'Jen','Charlie'}
+# art_friends = {'Rolf', 'Kamil', 'Jen'}
+# science_firends = {'Jen','Charlie'}
+
+# art_but_not_scienc = art_friends.difference(science_firends) #ten wiersz przekaze nam info o tym kto nie jest zarowno art i science_friendsem
+# science_firends_but_not_art = science_firends.difference(art_friends)
+
+# print(art_but_not_scienc)
+# print('\n')
+# print(science_firends_but_not_art)
+
+# print('\n')
+
+# not_in_both = art_friends.symmetric_difference(science_firends) #nie sa w oby setach
+# print(not_in_both)
+
+# art_and_science = art_friends.intersection(science_firends) #jesli chcemy uzyskac czlonkow obu setow
+# print(art_and_science)
+
+# all_friends = art_friends.union(science_firends) #wszystkie elementy bez powatrzajacych sie z obu setow
+# print(all_friends)
+
+
+
+# set_one = {1, 2, 3, 4, 5}
+# set_two = {1, 3, 5, 7, 9, 11}
+# set_three = set_one.intersection(set_two)
+# print(set_three)
+
+nearby_people = {'Rolf', 'Jen', 'Anna'}
+user_friends = set()  # This is an empty set, like {}
+
+name = input("please give us a name: ")
+user_friends.add(name)
+user_friends_01 = user_friends.intersection(nearby_people)
+print(user_friends_01)
---
+++ dictionaries.py
@@ -0,0 +1,25 @@
+# friend_ages = {'Rolf': 24, 'Adam': 25, 'Anne': 29} #slownik 3 elementowy
+# print(friend_ages,['Rolf'])
+
+# friend_ages['Bob'] = 20 #mozna zawsze dodac elementy do slownika 
+# friend_ages['Rolf'] = 29 #edytujemy wiek Rolfa
+
+# print(friend_ages)
+
+#tworzymy tuple ze slowinikiem dictionary w wewnatrz
+# friends = ( 
+#     {'name': 'Rolf Smith', 'age': 24},
+#     {'name': 'Adam  Wool', 'age': 28},
+#     {'name': 'Anne Pun', 'age': 25}
+# )
+
+# print(friends[0]['name']) #friends[0] - pobieramy pierwszy element slownika a pozniej name = Rolf Smith
+# print(friends[1]['age']) #pobieramy drugi element ze slownika a pozniej wiek
+# print(friends[2]['name']) #pobieramy drugi element ze slownika  a pozniej name
+
+
+friends = [('Rolf', 24), ('Adam', 25), ('Anne', 29)] #lista tuples 
+
+friend_ages = dict(friends) #zamieniamy tuple na slownik
+print(friend_ages)
+
