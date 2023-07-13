d5660af51be022d7be250f256b1f787286e289f4
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-13 16:15:27 +0200

    Destructuring

---
+++ destructuring.py
@@ -0,0 +1,7 @@
+# currencies = (0.8, 1.2) #tupla
+# usd, eur = currencies
+
+friends = [('Rolf', 25), ('Anne', 34), ('Kamil', 35)] #lista tupli/krotek
+
+for name, age in friends:
+    print(f'{name} is {age} years old.')
