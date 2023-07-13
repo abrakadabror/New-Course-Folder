46b965e3aeba951995c773c0e8bb17a0098b4eb5
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-13 14:04:39 +0200

    added the if_statment_lesson

---
+++ if_statmenst_in_python.py
@@ -0,0 +1,7 @@
+# pozwala na podjemowanie decyzji w zaleznosci od spelnienia warunku logicznego prawda/falsz
+
+friend = 'Rolf'
+user_name = input('What is your name: ')
+
+if True:
+    print("hello, friend!")
--- join.py
+++ join.py
@@ -2,5 +2,5 @@ friends = ['Rolf', 'Anne', 'Charlie']
 print(f"My friends are {friends}.")
 
 #zeby ladniej wyswietlic tworzymy nowa zmienna
-comma_separated = ', '.join(friends)
+comma_separated = ' & '.join(friends)
 print(f"My friends are {comma_separated}.")
