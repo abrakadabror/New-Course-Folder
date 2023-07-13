69bcfd102b093980c108904698cbd1ba450856d9
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-13 15:47:15 +0200

    a simple txt menu

---
+++ Exercise_a_simple_text_menu.py
@@ -0,0 +1,12 @@
+user_choice = input('Choose one from two options(p/q)')
+while user_choice != 'q':
+    if user_choice == 'p':
+        print('Hello')
+    user_choice = input('Again the same question ?')
+    
+        
+    
+
+
+
+    
