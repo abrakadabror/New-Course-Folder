5e77837b7117c7067aebf2d85ac6025345b4aa5c
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-13 13:59:06 +0200

    add join lesson

---
+++ join.py
@@ -0,0 +1,6 @@
+friends = ['Rolf', 'Anne', 'Charlie']
+print(f"My friends are {friends}.")
+
+#zeby ladniej wyswietlic tworzymy nowa zmienna
+comma_separated = ', '.join(friends)
+print(f"My friends are {comma_separated}.")
