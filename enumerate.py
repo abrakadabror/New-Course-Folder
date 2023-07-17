287eee98bbc46e71d3ed93e5cb1fc29e6b3dabf7
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-17 10:44:21 +0200

    enumerate

--- enumerate.py
+++ enumerate.py
@@ -1,16 +1,17 @@
 friends = ['Rolf', 'John', 'Anna']
 
-# index = 0
+index = 0
 
-# for friend in friends:
-#     print(friend)
-#     print(friend)
-#     index = index + 1
+for friend in friends:
+    print(index)
+    print(friend)
+    index = index + 1
 
-for index, friend in enumerate(friends):
+for index, friend in enumerate(friends, start=1):  # statrujemy od 1
     print(index)
     print(friend)
 
-print(list(enumerate(friends)))  # tworzy liste tupli/krotek
+print(list(enumerate(friends)))  # tworzy liste tupli
 
 print(list(zip([0, 1, 2], friends)))
+print(dict(enumerate(friends)))  # zamienia tupe w klucze i wartosci slownikowe
