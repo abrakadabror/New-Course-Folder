2763888de86404d913a375832da57c982ca2fc4b
Kamil <77681807+abrakadabror@users.noreply.github.com>
2023-07-17 08:18:12 +0200

    slicing  update

--- slicing_list.py
+++ slicing_list.py
@@ -1,3 +1,15 @@
 friends = ['Rolf', 'Charlie', 'Anna', 'Bob', 'Jen']
-print(friends[2:4]) #start list from 2 to 4 
-print(friends[-3:]) #start of the list from Anna and on Jen
+print(friends[2:4])  # zaczyna wymieniac od "Anna" i konczy na "Bob"
+print(friends[1:])  # wydrukuje wszystkich bez pierwszej pozycji
+print(friends[:4])  # wydrukuje wszystko bez ostatniej pozycji "Jen"
+print(friends[0:0])  # nie wydrukuje niczego
+# wydrukujemy wszystkie pozycje z listy friends jako nowa liste...
+print(friends[:])
+print(friends)
+
+print(friends[-3:])  # zaczynamy liczyc od konca czyli do "Anna"
+
+print(friends[-3:-1])  # wypisze element "Anna" do elementu "Bob"
+
+
+# najwazniejsza informacja to ta, ze pierwszy number to starting point a drugi to numer koncowy
